1caf2023d53f4f1922e32451c0729a46118eaa52
Yashank Bhola <64089101+yashankxy@users.noreply.github.com>
2023-07-24 20:38:01 -0400

    Added Menu

--- src/Functions/Controller.java
+++ src/Functions/Controller.java
@@ -19,4 +19,88 @@ public class Controller {
             db.disconnect();
         return true;
     }    
+
+    public void execute() {
+        if (sc != null && db != null) {
+            String val;
+            int choice;
+            do {
+                System.out.println("\n Options: \n"+
+                                "        1. Exit \n"+
+                                "        2. Login\n"+
+                                "        3. Sign-Up\n");
+                System.out.print("Select:");
+                val = sc.nextLine();
+                try {
+                    choice = Integer.parseInt(val);
+                    switch (choice) {
+                        case 1:
+                            break;
+                        case 2:
+                            // login()
+                            break;
+                        case 3:
+                            // sign-Up();
+                            break;
+                        default:
+                            System.out.println("Invalid option");
+                            break;
+                    }
+                } catch (NumberFormatException e) {
+                    val = "-1";
+                }
+            } while (!val.equals("1") && !val.equals("2") && !val.equals("3"));
+
+            if (val.equals("1")) close();
+
+            sc.close();
+        } 
+        else {
+            System.out.println("\nConnection Failed");
+        }
+    }
+
+    public void signup(){
+        if (sc != null && db != null){
+            String val;
+            int choice;
+            do {
+                System.out.println("\n Options: \n"+
+                                "        1. Exit \n"+
+                                "        2. sign up as Host\n"+
+                                "        3. Sign up as customer \n");
+                System.out.print("Select:");
+                val = sc.nextLine();
+                try {
+                    choice = Integer.parseInt(val);
+                    switch (choice) {
+                        case 1:
+                            break;
+                        case 2:
+                            // host()
+                            break;
+                        case 3:
+                            // customer();
+                            break;
+                        default:
+                            System.out.println("Invalid option");
+                            break;
+                    }
+                } catch (NumberFormatException e) {
+                    val = "-1";
+                }
+            } while (!val.equals("1") && !val.equals("2") && !val.equals("3"));
+         
+            if (val.equals("1")) close();
+            sc.close();
+        }    
+        else {
+            System.out.println("\nConnection Failed");
+        }
+    }
+
+    public void host(){
+        
+    }
+
 }
