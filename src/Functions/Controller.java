26f6c508f3033d7f5367262350ef48707da60cb3
Yashank Bhola <64089101+yashankxy@users.noreply.github.com>
2023-07-25 14:13:38 -0400

    Added Sign up for renter and customer

--- src/Functions/Controller.java
+++ src/Functions/Controller.java
@@ -2,10 +2,13 @@ package Functions;
 
 import java.util.Scanner;
 
+import Classes.Booking;
+
 public class Controller {
     Scanner sc = null;
     sqlFunctions db = null;
 
+    /*  Start */
     public boolean run (){
         if (sc == null)
             sc = new Scanner(System.in);
@@ -14,12 +17,14 @@ public class Controller {
             db.connect();
         return true;
     }
+    /** Shut down */
     public boolean close(){
         if (db != null)
             db.disconnect();
         return true;
     }    
-
+    
+    /** Opens up Menu */
     public void Menu() {
         if (sc != null && db != null) {
             String val;
@@ -60,6 +65,7 @@ public class Controller {
         }
     }
 
+    /** Sign up as a Host or customer account */
     public void signup(){
         if (sc != null && db != null){
             String val;
@@ -99,8 +105,90 @@ public class Controller {
         }
     }
 
-    public void host(){
+    /** Create a new customer account*/
+    public void customer(){
+        String name, email, password, dob, address, occup, sin;
+        // Getting user input
+        System.out.println("\nEnter your name: ");
+        name = sc.nextLine();
+        System.out.println("\nEnter your Date of Birth (dd/mm/yy): ");          // Add do while
+        dob = sc.nextLine();
+        System.out.println("\nEnter your address: ");          // Add do while
+        address = sc.nextLine();
+        System.out.println("\nEnter your occupation: ");
+        occup = sc.nextLine();
+        System.out.println("\nEnter your SIN number: ");          // Add do while
+        sin = sc.nextLine();
+        System.out.println("\nEnter your email: ");         // Add do while
+        email = sc.nextLine();
+        System.out.println("\nEnter your password: ");          // Add do while
+        password = sc.nextLine();
 
+        // Check if already exists
+            // if (db.checkCustomer(email, password)){
+            //     System.out.println("\nAccount already exists");
+
+        // Inserting into database
+            // db.insertCustomer(name, email, password, dob, address, occup, sin);
+        
+        System.out.println("\nAccount created successfully");
+        signup();
     }
+
+    /** Create a new host account*/
+    public void host(){
+        String name, email, password, dob, address, occup, sin;
+        String cc_num, cc_name, cc_exp, cc_cvv;
+        String rental_date, hostName, propertyDetails;
+
+        // Getting user input
+        System.out.println("\nEnter your name: ");
+        name = sc.nextLine();
+        System.out.println("\nEnter your Date of Birth (dd/mm/yy): ");          // Add do while
+        dob = sc.nextLine();
+        System.out.println("\nEnter your address: ");          // Add do while
+        address = sc.nextLine();
+        System.out.println("\nEnter your occupation: ");
+        occup = sc.nextLine();
+        System.out.println("\nEnter your SIN number: ");          // Add do while
+        sin = sc.nextLine();
+        System.out.println("\nEnter your email: ");         // Add do while
+        email = sc.nextLine();
+        System.out.println("\nEnter your password: ");          // Add do while
+        password = sc.nextLine();
+        //___________________________Renter Credit card Information________________________________
+        System.out.println("\nEnter your credit card number: ");          // Add do while
+        cc_num = sc.nextLine();
+        System.out.println("\nEnter your credit card name: ");          // Add do while
+        cc_name = sc.nextLine();
+        System.out.println("\nEnter your credit card expiry date (mm/yy): ");          // Add do while
+        cc_exp = sc.nextLine();
+        System.out.println("\nEnter your credit card cvv: ");          // Add do while
+        cc_cvv = sc.nextLine();
+        
+        //___________________________Renter History________________________________
+        System.out.println("\nEnter previous rental date: ");          // Add do while
+        rental_date = sc.nextLine();
+        System.out.println("\nEnter previous hostname: ");          // Add do while
+        hostName = sc.nextLine();
+        System.out.println("\nEnter previous propery details: ");          // Add do while
+        propertyDetails = sc.nextLine();
+
+        //___________________________Renter Future________________________________
+        // System.out.println("\nEnter available rental date: ");          // Add do while
+        // rental_date = sc.nextLine();
+        // System.out.println("\nEnter hostname: ");          // Add do while
+        // hostName = sc.nextLine();
+        // System.out.println("\nEnter previous propery details: ");          // Add do while
+        // propertyDetails = sc.nextLine();
+        
+        // Inserting into database
+            // db.insertRenter(name, email, password, dob, address, occup, sin);
+
+        System.out.println("\nAccount created successfully");
+        signup();
+    }
+
+
 
 }
