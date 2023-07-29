34ac771e9799824bc102c0c63aeb0c13830ba7b4
Yashank Bhola <64089101+yashankxy@users.noreply.github.com>
2023-07-29 08:25:30 -0400

    ER diag

--- src/App.java
+++ src/App.java
@@ -4,6 +4,10 @@ import Functions.sqlFunctions;
 import Functions.Controller;
 
 
+/*
+    ER Diagram: https://drive.google.com/file/d/1mkx9NiLk2uK2gIdeSuqhW2W41UT9KF79/view?usp=sharing
+*/
+
 public class App {
     public static void main(String[] args) throws Exception {
             Controller cmd = new Controller();
